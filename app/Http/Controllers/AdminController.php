0726b9a88e64bfb898dcd8fb170d85e57f6144a1
Mohammed Saeed <105430093+SA33ED@users.noreply.github.com>
2023-08-20 16:34:31 +0300

    full-dashboard

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    public function index($page){
+        if(view()->exists($page)){
+            return view($page);
+        }else{
+            return view('404');
+        }
+    }
+}
+
